a3b924a3073af5f812321025db03e11bf8d4d2fa
wujianchang <wujianchang@xiaoice.com>
2020-12-26 10:41:30 +0800

    init
    first useful version

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, make_response, jsonify
+from browser import firefoxdriver
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+f = firefoxdriver()
+
+
+@app.route('/judge_doc', methods=['GET'])
+def judge_doc():
+    key_word = request.args["query"]
+    data = f.get_data(key_word)
+    header = {
+        "Content-Type": "application/json;charset=utf-8"
+    }
+    return jsonify({"result": data}), 200, header
---
+++ browser/firefox.py
@@ -0,0 +1,109 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+import time
+
+
+INDEX_URL = "https://wenshu.court.gov.cn/"
+PHONE_NUM = "17621626837"
+PWD = "Qwe1994422."
+
+
+class firefoxdriver(object):
+    """
+    docstring
+    """
+
+    def __init__(self):
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+
+        self.__driver = webdriver.Firefox(options=options)
+        self.__wait = WebDriverWait(self.__driver, 20)
+        account, pwd = get_account_info()
+        self.__account = account
+        self.__pwd = pwd
+        self.__driver.get("https://www.baidu.com")
+        self.__login()
+
+    def get_data(self, key_word):
+        self.__login()
+        search = self.__wait_by_class("search-middle")
+        search = search.find_element_by_tag_name("input")
+        search.send_keys(key_word)
+        search.send_keys(Keys.ENTER)
+        items = self.__wait_by_class_batch("LM_list")
+        data = []
+        for i in items:
+            result = parse(i)
+            data.append(result)
+        self.__driver.get(INDEX_URL)  # 返回首页，重置搜索条件
+        return data
+
+    def __get_judge(self):
+        self.__driver.execute_script(
+            'window.open("https://wenshu.court.gov.cn/")')
+        window_list = self.__driver.window_handles
+        self.__driver.switch_to.window(window_list[1])
+
+    def __is_in_judge(self):
+        return INDEX_URL in self.__driver.current_url
+
+    def __is_logined(self):
+        login = self.__wait_by_id_batch("loginLi")
+        if len(login) == 0:
+            return False
+        return not login[0].text == "登录"
+
+    def __login(self):
+        self.__driver.refresh()
+        if not self.__is_in_judge():
+            self.__get_judge()
+        time.sleep(1)
+        if self.__is_logined():
+            return
+        login_url = INDEX_URL + self.__wait_by_id("loginLi").find_element_by_tag_name(
+            "a").get_attribute("onclick")[8:-2]
+        self.__driver.get(login_url)
+
+        frame = self.__wait_by_id("contentIframe")
+        self.__driver.switch_to.frame(frame)
+        phone = self.__wait_by_class("phone-number-input")
+        phone.send_keys(self.__account)
+        pwd = self.__wait_by_class("password")
+        pwd.send_keys(self.__pwd)
+        login = self.__wait_by_class("login-button-container")
+        login.click()
+        time.sleep(1)
+        self.__driver.switch_to.default_content()
+        time.sleep(1.5)
+
+    def __wait_by_class(self, class_name):
+        return self.__wait.until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
+
+    def __wait_by_class_batch(self, class_name):
+        return self.__wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, class_name)))
+
+    def __wait_by_id(self, id):
+        return self.__wait.until(EC.presence_of_element_located((By.ID, id)))
+
+    def __wait_by_id_batch(self, id):
+        return self.__wait.until(EC.presence_of_all_elements_located((By.ID, id)))
+
+
+def get_account_info():
+    return PHONE_NUM, PWD
+
+
+def parse(item):
+    return {
+        "doc_type": item.find_element_by_class_name("labelTwo").text,
+        "title": item.find_element_by_class_name("caseName").text,
+        "court": item.find_element_by_class_name("slfyName").text,
+        "doc_id": item.find_element_by_class_name("ah").text,
+        "date": item.find_element_by_class_name("cprq").text,
+        "reason": item.find_element_by_tag_name("p").text
+    }
